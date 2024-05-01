0e6e261286a1b24583d637448748be0802abf471
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-01 18:25:20 -0300

    first commit

---
+++ backend/src/main/java/com/gabriel/minhacasa/MinhacasaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.minhacasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhacasaApplication.class, args);
+	}
+
+}
