efde26b68a6a1144f75781b2202acdada3f19892
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-05 19:00:25 -0300

    finished basic homepage

--- frontend/minhacasa/src/app/components/homepage/homepage.component.ts
+++ frontend/minhacasa/src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { CardComponent } from '../layout/card/card.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent, CardComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
