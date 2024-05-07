7290f18d528bcd43ec6e9eaf04fc4e5da26d058e
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-07 11:15:31 -0300

    create loginpage

--- frontend/minhacasa/src/app/app.routes.ts
+++ frontend/minhacasa/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
 
 export const routes: Routes = [
     {
         path: '', component: HomepageComponent
+    },
+    {
+        path: 'login', component: LoginpageComponent
     }
 ];
