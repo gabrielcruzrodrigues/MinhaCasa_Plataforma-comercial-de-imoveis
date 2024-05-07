b5585ba291d3699122277cf5fcd8dad136b9bff0
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-07 11:41:10 -0300

    refactor homepage background and create basic register page

--- frontend/minhacasa/src/app/app.routes.ts
+++ frontend/minhacasa/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
 
 export const routes: Routes = [
     {
@@ -8,5 +9,8 @@ export const routes: Routes = [
     },
     {
         path: 'login', component: LoginpageComponent
+    },
+    {
+        path: 'register', component: RegisterpageComponent
     }
 ];
