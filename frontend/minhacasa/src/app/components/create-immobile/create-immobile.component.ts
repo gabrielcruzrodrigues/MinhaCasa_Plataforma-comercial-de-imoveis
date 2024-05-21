56d8855e148029b5fbe98d463ec66fcedfb956ce
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-21 10:47:29 -0300

    create container for elements in createimmobile page

--- frontend/minhacasa/src/app/components/create-immobile/create-immobile.component.ts
+++ frontend/minhacasa/src/app/components/create-immobile/create-immobile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { FooterComponent } from '../layout/footer/footer.component';
 
 @Component({
   selector: 'app-create-immobile',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './create-immobile.component.html',
   styleUrl: './create-immobile.component.scss'
 })
