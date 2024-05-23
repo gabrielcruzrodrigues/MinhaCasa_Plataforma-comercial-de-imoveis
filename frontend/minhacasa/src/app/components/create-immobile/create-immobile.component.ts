2b85d1ca0fee4cb17354103ff88608e356819654
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-05-23 11:29:06 -0300

    create basic carrosel

--- frontend/minhacasa/src/app/components/create-immobile/create-immobile.component.ts
+++ frontend/minhacasa/src/app/components/create-immobile/create-immobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -10,13 +10,16 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-immobile',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, ReactiveFormsModule, FormsModule, ModalAlertComponent, CommonModule, ModalTextComponent],
+  imports: [
+    NavbarComponent, FooterComponent, ReactiveFormsModule, FormsModule, ModalAlertComponent, CommonModule, ModalTextComponent,
+  ],
   templateUrl: './create-immobile.component.html',
   styleUrl: './create-immobile.component.scss'
 })
 export class CreateImmobileComponent implements OnInit{
 
   form: FormGroup;
+  @ViewChild('imageContainer', { static: false }) imageContainer!: ElementRef<HTMLDivElement>;
 
   constructor(
     private fb: FormBuilder,
@@ -94,5 +97,26 @@ export class CreateImmobileComponent implements OnInit{
 
   submit():void {
     console.log(this.form.value);
+  }
+
+  onFileChange(event: any):void {
+    const files = event.target.files as FileList;
+    const container = this.imageContainer.nativeElement;
+    container.innerHTML = ''; //limpa a div antes de adicionar novas imagens
+
+    Array.from(files).forEach((file: File) => {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        img.style.width = '80%'; // Define o tamanho das imagens
+        img.style.height = '80%'; // Define o tamanho das imagens
+        img.style.margin = '0px'; // Adiciona margem entre as imagens
+        img.style.objectFit = 'contain'
+        container.appendChild(img);
+      };
+      reader.readAsDataURL(file);
+    });
+
   }
 }
