3376e64360da1538f2c098d76e677eff9bd672ce
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-08-05 11:09:30 -0300

    created basic plans and services page

---
+++ backend/src/test/java/com/gabriel/minhacasa/controller/MessageControllerTest.java
@@ -0,0 +1,45 @@
+package com.gabriel.minhacasa.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageControllerTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void test1() {
+    }
+
+    @Test
+    void createMessage() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByType() {
+    }
+
+    @Test
+    void findBySenderName() {
+    }
+
+    @Test
+    void deleteMessage() {
+    }
+}
