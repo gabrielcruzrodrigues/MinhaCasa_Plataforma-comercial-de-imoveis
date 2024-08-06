623aa401f9d52d98d472cecc0cce58c88797eb70
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-08-06 11:29:24 -0300

    finished plans and services page

--- frontend/src/app/components/plans-and-services/plans-and-services.component.ts
+++ frontend/src/app/components/plans-and-services/plans-and-services.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { FooterComponent } from '../layout/footer/footer.component';
 
 @Component({
   selector: 'app-plans-and-services',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent, FooterComponent
   ],
   templateUrl: './plans-and-services.component.html',
   styleUrl: './plans-and-services.component.scss'
